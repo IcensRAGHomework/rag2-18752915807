71ae22bbeca87c15fae85bc49bef7f76e9e87f73
Ricardo1_Yue <Ricardo1_Yue@asus.com>
2025-01-24 10:51:19 +0800

    test5
    Signed-off-by: Ricardo1_Yue <Ricardo1_Yue@asus.com>

--- student_assignment.py
+++ student_assignment.py
@@ -19,40 +19,26 @@ def hw02_2(pdf_path):
     loader = PyPDFLoader(pdf_path)
     documents = loader.load()
 
+    combined_text = "".join([doc.page_content for doc in documents])
+    # print(combined_text)
     separators = [
-        r" 第\s*[一二三四五六七八九十百千]+\s*章 ",
-        r"第\s*\d+\s*條 ",
-        # r"第\s*\d+-\d+\s*條",
-        r"\n第\s*\d+-\d+\s*條",
+        r"\s第\s*[一二三四五六七八九十]+\s*章\s",
+        r"第\s*\d+\s*條",
+        r"\s第\s*\d+\s*條",
+        r"第\s*\d+-\d+\s*條",
     ]
     splitter = RecursiveCharacterTextSplitter(
         separators=separators,
-        chunk_size=80,
+        chunk_size=50,
         chunk_overlap=0,
         is_separator_regex=True
     )
 
-    chunks = splitter.split_documents(documents)
-    #
-    # page_chunk_counts = {}
-    # for chunk in chunks:
-    #     page_number = chunk.metadata.get('page', 'Unknown')
-    #     if page_number in page_chunk_counts:
-    #         page_chunk_counts[page_number] += 1
-    #     else:
-    #         page_chunk_counts[page_number] = 1
-    #
-    # chunk_counts_list = [page_chunk_counts.get(page, 0) for page in range(len(documents))]
-    #
+    chunks = splitter.split_text(combined_text)
     for i, chunk in enumerate(chunks):
-        page_number = chunk.metadata.get('page', 'Unknown')
-        print(f"Chunk {i + 1} (Page {page_number + 1}):\n"
-              f"{chunk.page_content}\n"
+        print(f"Chunk {i + 1}:\n"
+              f"{chunk}\n"
               f"{'-' * 40}")
-    #
-    # for page, count in enumerate(chunk_counts_list):
-    #     print(f"Page {page + 1} has {count} chunks")
-
     return len(chunks)
 
 if __name__ == "__main__":
